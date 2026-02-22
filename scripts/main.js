60661a36b17bace5629d726b9360b3df57051b8d
BaelongandiAendo <baelongandiaendo@gmail.com>
2026-02-22 17:13:26 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+let bouton = document.querySelector("#bouton-alerte");
+
+
+if(bouton) {
+    bouton.addEventListener("click", function() {
+        alert("Bienvenue au Palm Beach ! Profitez de votre visite sur notre site.");
+    });
+}
+
